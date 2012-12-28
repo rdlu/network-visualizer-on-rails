17e9cbb2d5ea6937dffc898bab8f0dfef58936e0
Rodrigo Dlugokenski <rddweb@gmail.com>
2012-12-28 12:31:19 -0200

    Esqueleto basico do projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MomRails::Application.config.secret_token = 'aa4081c434c00dc96fb31210a091d394b173d3a50191d5eeb04f5767c63dfc768eb9cb73455335a0cea8956291b92e0a3e43850ccce07a3e107d05c5b634ea69'
