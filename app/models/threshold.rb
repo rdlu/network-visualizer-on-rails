9dc482171df96980e60813b7a72536f11729070b
Rodrigo Dlugokenski <rddweb@gmail.com>
2013-01-30 12:29:28 -0200

    Cadastro de novo limiar

--- app/models/threshold.rb
+++ app/models/threshold.rb
@@ -1,6 +1,26 @@
+# coding: utf-8
 class Threshold < ActiveRecord::Base
   attr_accessible :compliance_level, :compliance_period, :compliance_method, :goal_level, :goal_method, :goal_period, :name
 
   belongs_to :connection_profile
   belongs_to :metric
+
+  def goal_methods
+    [%w(Mediana median),
+    ["Valor Bruto","raw"]]
+  end
+
+  def goal_periods
+    [%w(Diária-PMT daily-rush),
+    %w(Diária daily),
+    %w(Individual each)]
+  end
+
+  def compliance_methods
+    [["Quociente simples","quotient"]]
+  end
+
+  def compliance_periods
+    [%w(Mensal monthly)]
+  end
 end
