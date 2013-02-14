0798572d07c4b10207f06572497e858cb31957af
Rodrigo Dlugokenski <rddweb@gmail.com>
2013-02-14 19:05:42 -0200

    Assets faltando no precompile para o modo production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,9 @@ MomRails::Application.configure do
   config.assets.digest = true
 
   # Defaults to nil and saved in location specified by config.assets.prefix
-  config.assets.manifest = '/mom'
+  # config.assets.manifest = YOUR_PATH
+
+  config.assets.prefix = '/mom/assets'
 
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
@@ -46,7 +48,7 @@ MomRails::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( jsoneditor.js map.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
