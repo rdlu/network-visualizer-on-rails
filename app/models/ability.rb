ca24e9eafd6516e3a75ce5254304139d03a011be
Pedro Vanzella <pedro@pedrovanzella.com>
2013-02-27 14:50:07 -0300

    Permissões de gerente e visualizador.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,10 +7,16 @@ class Ability
     if user.role? :admin
       can :manage, :all
     elsif  user.role? :normal
-     can :read, :all
-     can :manage, Probe
-     can :destroy, user
-     can :update, user
+      can :read, :all
+      can :manage, Probe
+      can :manage, Schedule
+      can :manage, Profile
+      can :manage, Reports
+      can :destroy, user
+      can :update, user
+    elsif user.role? :visualizador
+      can :read, :all
+      can :manage, user
     end
   end
 
