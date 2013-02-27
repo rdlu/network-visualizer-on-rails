516ea19c1588b05422c61be82a0fcc61cb367421
Rodrigo Dlugokenski <rddweb@gmail.com>
2013-02-27 13:34:35 -0300

    Um pouco sobre medianas e também um approach para o autorestart do delayedjob

--- app/models/median.rb
+++ app/models/median.rb
@@ -3,7 +3,7 @@ class Median < ActiveRecord::Base
   belongs_to :threshold
   attr_accessible :end_timestamp, :expected_points, :schedule_uuid, :start_timestamp, :total_points, :dsavg, :sdavg
 
-  def calculate
+  def calculate (schedule, threshold, start_timestamp, end_timestamp)
     #TODO: calcular metricas anatel que sao do tipo mediana
   end
 end
