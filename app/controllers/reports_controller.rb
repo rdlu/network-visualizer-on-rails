24ba337cc9caf9404fc2082eb2f438a62cb10fcd
Pedro Vanzella <pedro@pedrovanzella.com>
2013-03-04 14:18:16 -0300

    Gerando csv. Precisa de um refatoramento sério.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -53,12 +53,76 @@ class ReportsController < ApplicationController
   end
 
   def csv
-    @result = CSV.generate do |csv|
-      csv << ["titles", "go", "here"]
+    source = Probe.find(params[:source][:id])
+    destination = Probe.find(params[:destination][:id])
+    metric = Metric.find(params[:metric][:id])
+    schedule = Schedule.where(:destination_id => destination.id).where(:source_id => source.id).all.last
+
+    from = DateTime.parse(params[:date][:start]+' '+params[:time][:start]+' '+DateTime.current.zone).in_time_zone
+    to = DateTime.parse(params[:date][:end]+' '+params[:time][:end]+' '+DateTime.current.zone).in_time_zone
+
+    raw_results = Results.
+        where(:schedule_id => schedule.id).
+        where(:metric_id => metric.id).
+        where(:timestamp => from..to).order('timestamp ASC').all
+
+    @end_csv = CSV.generate do |csv|
+      csv << [# source
+              "source name",
+              "source status",
+              "source location",
+              # destination
+              "destination name",
+              "destination status",
+              "destination location",
+              # metric
+              "metric name",
+              # schedule
+              "schedule start",
+              "schedule end",
+              "schedule polling",
+              "schedule status",
+              # date
+              "start date",
+              "end date",
+              # result
+              "x",
+              unless metric.plugin == 'rtt' then "dsavg" end,
+              unless metric.plugin == 'rtt' then  "sdavg" end,
+              if metric.plugin == 'rtt' then "y" end,
+              "extra"
+      ]
+      raw_results.each do |result|
+        csv << [# source
+                source.pretty_name,
+                source.pretty_status,
+                "#{source.city}/#{source.state.upcase}",
+                # destination
+                destination.pretty_name,
+                destination.pretty_status,
+                "#{destination.city}/#{destination.state.upcase}",
+                # metric
+                metric.name,
+                # schedule
+                schedule.start,
+                schedule.end,
+                schedule.polling,
+                schedule.status,
+                # date
+                from,
+                to,
+                # result
+                result.x,
+                unless metric.plugin == 'rtt' then result.dsavg end,
+                unless metric.plugin == 'rtt' then result.sdavg end,
+                if metric.plugin == 'rtt' then result.y end,
+                result.extra
+        ]
+      end
     end
 
     respond_to do |format|
-      format.csv { render text: @result }
+      format.csv { render text: @end_csv }
     end
   end
 end
