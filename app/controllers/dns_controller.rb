1cdae5e73ee19e06c42ba2e0f2ffbd7e57335341
Pedro Vanzella <pedro@pedrovanzella.com>
2013-03-06 15:43:27 -0300

    Index action for DNS.

--- app/controllers/dns_controller.rb
+++ app/controllers/dns_controller.rb
@@ -1,5 +1,14 @@
 class DnsController < ApplicationController
+  before_filter :authenticate_user!
   def index
+    authorize! :read, self
+
+    @dns_list = Dns.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @dns_list }
+    end
   end
 
   def show
