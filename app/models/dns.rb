2bdee9160e53f322422f9ce584d4b62c87048d11
Pedro Vanzella <pedro@pedrovanzella.com>
2013-03-07 16:00:24 -0300

    Limpando model antigo

--- app/models/dns.rb
+++
@@ -1,3 +0,0 @@
-class Dns < ActiveRecord::Base
-  attr_accessible :address, :internal, :name, :primary, :vip
-end
--- test/unit/dns_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class DnsTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
