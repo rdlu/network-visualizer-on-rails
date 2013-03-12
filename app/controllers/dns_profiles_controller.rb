3b22612186822e1cb403984962f6c710b688761c
Pedro Vanzella <pedro@pedrovanzella.com>
2013-03-12 13:24:54 -0300

    Bugfix

--- app/controllers/dns_profiles_controller.rb
+++ app/controllers/dns_profiles_controller.rb
@@ -15,11 +15,16 @@ class DnsProfilesController < ApplicationController
     authorize! :manage, self
     @profile = Profile.find(params[:id])
     @nameservers = Nameserver.all
+
+    unless @profile.config_method == "dns"
+      redirect_to edit_profile_path(@profile)
+    end
   end
 
   def create
     authorize! :manage, self
 
+    params[:profile][:nameservers] ||= []
     @profile = Profile.new(params[:profile])
     @profile.config_method = "dns"
 
@@ -33,10 +38,36 @@ class DnsProfilesController < ApplicationController
   end
 
   def show
+    authorize! :manage, self
+    @profile = Profile.find(params[:id])
 
+    respond_to do |format|
+      unless @profile.config_method == "dns"
+        redirect_to profile_path(@profile)
+      else
+        format.html
+        format.json { render json: @profile }
+      end
+    end
   end
 
   def update
+    authorize! :manage, self
+    params[:profile][:metric_ids] ||= []
+    @profile = Profile.find(params[:id])
 
+    respond_to do |format|
+      unless @profile.config_method == "dns"
+        redirect_to update_profile_path(@profile)
+      else
+        if @profile.update_attributes(params[:profile])
+          format.html { redirect_to dns_profile_path(@profile), notice: 'Updated!' }
+          format.json { head :no_content }
+        else
+          format.html { render action: 'edit' }
+          format.json { render json: @profile.errors, status: :unprocessable_entity }
+        end 
+      end
+    end
   end
 end
