05941ebb9f10d2ccd6eeb0ac7309d555b618dffc
Pedro Vanzella <pedro@pedrovanzella.com>
2013-03-14 12:40:24 -0300

    Criando novos sites no delayed job caso não existam ainda.

--- app/jobs/update_top_100_job.rb
+++ app/jobs/update_top_100_job.rb
@@ -22,8 +22,24 @@ class UpdateTop100
       # find the container
       doc.css('div.desc-container').each do |site|
         # the <h2> has the site name
+        site_name = site.css('h2').first.content
         # the topsites-label class has the URL
-        puts "#{site.css('h2').first.content} - #{site.css('span.topsites-label').first.content}"
+        site_url = site.css('span.topsites-label').first.content
+
+        site = Site.find_by_url(site_url)
+
+        unless site
+          Yell.new(:gelf, :facility => 'netmetric').info "[Top100] Criando site #{site_name} com URL #{site_url}"
+          site = Site.new(url: site_url, vip: false)
+
+          if site.save
+            Yell.new(:gelf, :facility => 'netmetric').info "[Top100] Site #{site_name} criado corretamente"
+          else
+            Yell.new(:gelf, :facility => 'netmetric').info "[Top100] Site #{site_name} não pode ser criado!"
+          end
+        else
+            Yell.new(:gelf, :facility => 'netmetric').info "[Top100] Site #{site_name} já existe. Pulando."
+        end
       end
     end
   end
