c92d8a348c5ee9a9764e4930a011cf55b09a2be8
Rodrigo Dlugokenski <rddweb@gmail.com>
2013-05-13 15:18:30 -0300

    Exportação de Gráficos em PNG/PDF/etc Já foi instalado no server da vivo o que era preciso.

--- app/assets/javascripts/reports.js
+++ app/assets/javascripts/reports.js
@@ -1,5 +1,6 @@
 //= require highcharts
 //= require highcharts/highcharts-more
+//= require highcharts/modules/exporting
 //= require jquery_imask
 //= require bootstrap_datepicker
 //= require moment-js/moment
