7dbc360a3c7129611b173e654d25e162f409d2df
Saymon Della Flora <saymondella@gmail.com>
2013-07-05 16:48:52 -0300

    Ajustes DNS: frontend

--- app/models/nameserver.rb
+++ app/models/nameserver.rb
@@ -1,7 +1,20 @@
+# coding: utf-8
 class Nameserver < ActiveRecord::Base
   attr_accessible :address, :internal, :name, :primary, :vip
 
+  validates :address, :presence => true, :length => {:maximum => 255, :minimum => 3}, :uniqueness => true
+  validate  :validate_address
+
   def pretty_name
     "#{self.name} (#{self.address})"
+  end
+
+
+  def validate_address
+    errors.add(:ipaddress, ' tem um formato inválido') unless ip?
+  end
+
+  def ip?
+    IPAddress.valid? self.address
   end
 end
