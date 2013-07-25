03c26687122fd0c7efad9a93cf99456c875a35a4
Pedro Vanzella <pedro@pedrovanzella.com>
2013-07-25 14:24:26 -0300

    Adiciona root ao XML do Profile; Salve XML

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -19,106 +19,145 @@ class Profile < ActiveRecord::Base
 
   def manager_ip
       h = load_hash_from_xml
-      h['manager-ip']
+      h['NMAgent']['manager-ip']
   end
 
   def manager_ip=(m)
+      h = load_hash_from_xml
+      h['NMAgent']['manager-ip'] = m
+      save_xml_from_hash(h)
   end
 
   def timeout
       h = load_hash_from_xml
-      h['timeout']
+      h['NMAgent']['timeout']
   end
 
   def timeout=(t)
+      h = load_hash_from_xml
+      h['NMAgent']['timeout'] = t
+      save_xml_from_hash(h)
   end
 
   def port
       h = load_hash_from_xml
-      h['port']
+      h['NMAgent']['port']
   end
 
   def port=(p)
+      h = load_hash_from_xml
+      h['NMAgent']['port'] = p
+      save_xml_from_hash(h)
   end
 
   def protocol
       h = load_hash_from_xml
-      h['protocol']
+      h['NMAgent']['protocol']
   end
 
   def protocol=(p)
+      h = load_hash_from_xml
+      h['NMAgent']['protocol'] = p
+      save_xml_from_hash(h)
   end
 
   def train_count
       h = load_hash_from_xml
-      h['train-count']
+      h['NMAgent']['train-count']
   end
 
   def train_count=(tc)
+      h = load_hash_from_xml
+      h['NMAgent']['train-count'] = tc
+      save_xml_from_hash
   end
 
   def probe_size
       h = load_hash_from_xml
-      h['probe-size']
+      h['NMAgent']['probe-size']
   end
 
   def probe_size=(ps)
+      h = load_hash_from_xml
+      h['NMAgent']['probe-size'] = ps
+      save_xml_from_hash(h)
   end
 
   def train_len
       h = load_hash_from_xml
-      h['train-len']
+      h['NMAgent']['train-len']
   end
 
   def train_len=(t)
+      h = load_hash_from_xml
+      h['NMAgent']['train-len'] = t
+      save_xml_from_hash(h)
   end
 
   def gap_value
       h = load_hash_from_xml
-      h['gap-value']
+      h['NMAgent']['gap-value']
   end
 
   def gap_value=(gv)
+      h = load_hash_from_xml
+      h['NMAgent']['gap-value'] = gv
+      save_xml_from_hash(h)
   end
 
   def time_mode
       h = load_hash_from_xml
-      h['time-mode']
+      h['NMAgent']['time-mode']
   end
 
   def time_mode=(tm)
+      h = load_hash_from_xml
+      h['NMAgent']['time-mode'] = tm
+      save_xml_from_hash(h)
   end
 
   def max_time
       h = load_hash_from_xml
-      h['max-time']
+      h['NMAgent']['max-time']
   end
 
   def max_time=(mt)
+      h = load_hash_from_xml
+      h['NMAgent']['max-time'] = mt
+      save_xml_from_hash(h)
   end
 
   def num_conexoes
       h = load_hash_from_xml
-      h['num-conexoes']
+      h['NMAgent']['num-conexoes']
   end
 
   def num_conexoes=(nc)
+      h = load_hash_from_xml
+      h['NMAgent']['num-conexoes'] = nc
+      save_xml_from_hash(h)
   end
 
   def ignore_gap
       h = load_hash_from_xml
-      h['ignore-gap']
+      h['NMAgent']['ignore-gap']
   end
 
   def ignore_gap(ig)
+      h = load_hash_from_xml
+      h['NMAgent']['ignore-gap'] = ig
+      save_xml_from_hash(h)
   end
 
   def plugins
       h = load_hash_from_xml
-      h['plugins']
+      h['NMAgent']['plugins']
   end
 
   def plugins=(ps)
+      h = load_hash_from_xml
+      h['NMAgent']['plugins'] = ps
+      save_xml_from_hash
   end
 
   def type_test
@@ -130,7 +169,7 @@ class Profile < ActiveRecord::Base
 
   def source_probe
       h = load_hash_from_xml
-      p = Probe.where(id: h['agt-index'])
+      p = Probe.where(id: h['NMAgent']['agt-index'])
       p
   end
 
@@ -193,10 +232,19 @@ class Profile < ActiveRecord::Base
   private
 
   def load_hash_from_xml
-      XmlSimple.xml_in(self.config_parameters, { 'KeepRoot' => true }) if self.config_method == "raw_xml" else {}
+      if self.config_method == "raw_xml"
+          if self.config_parameters == ""
+              self.config_parameters = "<NMAgent></NMAgent>"
+          end
+          XmlSimple.xml_in(self.config_parameters, { 'KeepRoot' => true })
+      else
+          {}
+      end
   end
 
   def save_xml_from_hash(h)
-    self.config_parameters = XmlSimple.xml_out(h, { 'KeepRoot' => true }) if self.config_method == "raw_xml"
+      if self.config_method == "raw_xml"
+          self.config_parameters = XmlSimple.xml_out(h, { 'KeepRoot' => true })
+      end
   end
 end
