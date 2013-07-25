fcd4c26280971b555f1df7d42f81655eab587278
Rodrigo Dlugokenski <rddweb@gmail.com>
2013-07-25 14:30:08 -0300

    Metric types validos no model Reports usa dygraphs nos dados brutos para qualquer periodo (ainda necessita limpeza de codigo legado). Gemfile atualizado e testado.

--- app/models/metric.rb
+++ app/models/metric.rb
@@ -1,5 +1,5 @@
 class Metric < ActiveRecord::Base
-  attr_accessible :description, :name, :order, :plugin, :reverse, :db_unit, :view_unit
+  attr_accessible :description, :name, :order, :plugin, :reverse, :db_unit, :view_unit, :metric_type
 
   validates :name, :presence => true, :length => {:maximum => 30, :minimum => 3}, :uniqueness => true
   validates :plugin, :presence => true, :length => {:maximum => 20, :minimum => 3}, :uniqueness => true,
@@ -29,5 +29,15 @@ class Metric < ActiveRecord::Base
 
   def view_unit= (value)
     self[:view_unit] = value.gsub(/bps|Bps/,'bps' => 'b/s', 'Bps' => 'B/s')
+  end
+
+  def metric_types
+    [
+        ['Métricas ativas','active'],
+        ['DNS','dns'],
+        ['DNS Errors','dns_detail'],
+        ['Carga Web','web_load'],
+        ['KPI','kpi']
+    ]
   end
 end
