f12204e200290b7846ef691e5887c5ed3c7a9175
Anderson Foscarini <anderson@foscarini.biz>
2013-09-09 16:04:46 -0300

    relatorios pac-man: introdução do design da pagina, com graficos fixos e tabela vazia

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2632,6 +2632,8 @@ class ReportsController < ApplicationController
   end
 
   def pacman
-     respond_to
+    respond_to do |format|
+      format.html { render :layout => false }
+    end
   end
 end
